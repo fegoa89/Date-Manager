f4ff745061f117c4499b24cc2602bcb632c7f438
Fernando <fgonzalezaguilera@gmail.com>
2015-03-12 22:24:17 +0100

    WIP first version running

--- lib/date_manager/date_parser.rb
+++ lib/date_manager/date_parser.rb
@@ -4,21 +4,21 @@ class DateParser
   # MDY -> mm/dd/yyyy  Date.strptime("6/15/2012", '%m/%d/%Y') - ^\d{2}(-|.|\/)\d{2}(-|.|\/)\d{4}$
   # DMY -> dd/mm/yyyy  Date.strptime("15/6/2012", '%d/%m/%Y') - ^\d{2}(-|.|\/)\d{2}(-|.|\/)\d{4}$
   # YMD -> yyyy/mm/dd  Date.strptime("2012/6/15", '%Y/%m/%d') - ^\d{4}(-|.|\/)\d{2}(-|.|\/)\d{2}$ with seconds '^\d{4}(-|.|\/)\d{2}(-|.|\/)\d{2} \d{2}:\d{2}:\d{2}$/', '2008-09-01 12:35:45'
+  # DateParser.new('02/02/2012','02/02/2013', 'DMY')
+  attr_accessor :start_date, :finish_date
 
-  DATE_FORMAT = ['MDY', 'DMY', 'YMD']
-  COMPONENT_SEPARATOR = ['']
+  OTHER_COMPONENT_SEPARATOR = {'.' => '/', '-' => '/'}
+  DATE_FORMAT               = ['MDY', 'DMY', 'YMD']
+
   # TODO date format hash with structure {format: 'MDY , structure: '%m/%d/%Y' }
   def initialize(start_date, finish_date, format)
-    @format      = get_format
+    @format      = get_format(format)
     @start_date  = start_date
     @finish_date = finish_date
   end
 
   def parse
     if valid_country_format_date?
-      parse_date
-    else
-      # Parse with standard format YYY/MM/DD
       parse_date
     end
   end
@@ -29,27 +29,34 @@ class DateParser
     DATE_FORMAT.include?(@format)
   end
 
-  def get_format
-    format.empty? ? 'YMD' : @format.upcase
-  end
-
-  def parse_date
-    case @format
-      when 'MDY'
-        if has_correct_structure?(date_format_structure[:mdy][:regex])
-        end
-      when 'DMY'
-
-      else
-    end
+  def get_format(format)
+    format.nil? ? 'YMD' : format.upcase
   end
 
   def has_correct_structure?(regex)
     @start_date =~ regex && @start_date =~ regex
   end
 
-  def parse_mdy_date
-    Date.strptime("6/15/2012", '%m/%d/%Y')
+  def parse_date
+    case @format
+      when 'MDY'
+        validate_string(date_format_structure[:mdy])
+      when 'DMY'
+        validate_string(date_format_structure[:dmy])
+      else
+        validate_string(date_format_structure[:ymd])
+    end
+  end
+
+  def validate_string(date_format)
+    if has_correct_structure?(date_format[:regex])
+      clean_date_separator
+      convert_to_date_object(date_format[:structure])
+    end
+  end
+
+  def clean_date_separator
+    # TODO
   end
 
   def date_format_structure
@@ -58,6 +65,10 @@ class DateParser
       dmy: { structure: '%d/%m/%Y', regex: /^\d{2}(-|.|\/)\d{2}(-|.|\/)\d{4}$/ },
       ymd: { structure: '%Y/%m/%d', regex: /^\d{4}(-|.|\/)\d{2}(-|.|\/)\d{2}$/ }
     }
+  end
+
+  def convert_to_date_object(structure)
+    [ Date.strptime(@start_date, structure), Date.strptime(@finish_date, structure) ]
   end
 
 end
